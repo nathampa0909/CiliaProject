6b502c7067f6a2915b5520445fa1c5bc38004444
Nathan <nat.hando@hotmail.com>
2020-02-01 22:20:40 -0300

    Chart, vehicle and location added.

--- _js/index.js
+++ _js/index.js
@@ -1,29 +1,34 @@
-$(document).ready(function () {
-    var trigger = $('.hamburger_open'),
-        overlay = $('.overlay'),
-        isClosed = false;
-
-    trigger.click(function () {
-        hamburger_open();
+$(document).ready(function() {
+    Highcharts.chart('chart', {
+        chart: {
+            type: 'line'
+        },
+        title: {
+            text: 'Brands'
+        },
+        subtitle: {
+            text: 'Source: loremipsum.com'
+        },
+        xAxis: {
+            categories: ['BMW', 'Chevrolet', 'Citroen', 'Volkswagen', 'Renalt', 'Jeep', 'Ferrari', 'Iveco', 'Hyundai', 'Honda', 'Ford', 'Fiat']
+        },
+        yAxis: {
+            title: {
+                text: 'Percentage (%)'
+            }
+        },
+        plotOptions: {
+            line: {
+                dataLabels: {
+                    enabled: true
+                },
+                enableMouseTracking: false
+            }
+        },
+        series: [{
+            name: 'Quantity',
+            data: [7.0, 6.9, 9.5, 14.5, 18.4, 21.5, 25.2, 26.5, 23.3, 18.3, 13.9, 9.6]
+        }]
     });
-
-    function hamburger_cross() {
-
-        if (isClosed == true) {
-            overlay.hide();
-            trigger.removeClass('is-open');
-            trigger.addClass('is-closed');
-            isClosed = false;
-        } else {
-            overlay.show();
-            trigger.removeClass('is-closed');
-            trigger.addClass('is-open');
-            isClosed = true;
-        }
-    }
-
-    function hamburger_open() {
-
-    }
 
 });
